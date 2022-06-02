3fa69c738ea9f6e6c36f2513c9fd2b19d22b39b9
Skyler-a <skyarthur352@gmail.com>
2022-06-02 11:55:08 -0300

    Chore: Database conection and schema add

---
+++ src/app/schema/carSchema.js
@@ -0,0 +1,33 @@
+const { required } = require('joi')
+const mongoose = require('mongoose')
+
+const carSchema = new mongoose.Schema({
+
+    model: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: String,
+        required: true
+    },
+    accessories: [{
+        type: String,
+        required: true
+    }],
+    passengersQtd: {
+        type: Number,
+        required: true
+    }
+})
+
+const car = mongoose.model('car', carSchema)
+module.exports = car
---
+++ src/infra/database/mongo/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+class Database {
+    constructor() {
+        this.connect();
+    }
+
+    connect() {
+        return mongoose.connect(`mongodb+srv://Compass:Compass134@desafio-final.ovbf0hj.mongodb.net/?retryWrites=true&w=majority`)
+    }
+} module.exports = new Database()
