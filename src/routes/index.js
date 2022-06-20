7951e24ed8b18ab22a2b68f9d2752235e1c3ade8
Skyler-a <skyarthur352@gmail.com>
2022-06-20 13:58:09 -0300

    Fix: fixed names in portuguese

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 const { Router } = require('express')
-const carros = require('./CarRoutes')
-const pessoas = require('./PersonRoutes')
+const car = require('./CarRoutes')
+const person = require('./PersonRoutes')
 const auth = require('./authRoutes')
 const rental = require('./rentalRoutes')
 
 module.exports = (server) => {
     server.use((req, res, next) => {
-        carros(server, new Router());
-        pessoas(server, new Router());
+        car(server, new Router());
+        person(server, new Router());
         auth(server, new Router());
         rental(server, new Router());
         next();
