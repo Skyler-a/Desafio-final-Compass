ae5e00c0a476bd40c8b8f78bcbcf8de43e6eb275
Skyler-a <skyarthur352@gmail.com>
2022-06-21 18:47:44 -0300

    Chore and Feat: DotEnv added and Heroku conection created

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 const app = require('./app')
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor rodando na porta 3000')
 })
