fd8d9cfe4a31526decf610c1b69f4487b9f3c42c
Skyler-a <skyarthur352@gmail.com>
2022-06-21 12:42:46 -0300

    Feat: created reserve schema

---
+++ src/app/schema/reserveSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+const reserveSchema = new mongoose.Schema({
+    id_user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "person",
+        required: true
+    },
+    data_start: {
+        type: String,
+        required: true
+    },
+    data_end: {
+        type: String,
+        required: true
+    },
+    id_car: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "car",
+        required: true
+    },
+    id_rental: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "rental",
+        required: true
+    },
+    final_value: {
+        type: Number,
+    }
+},
+    { timestamps: false, versionKey: false }
+);
+
+reserveSchema.plugin(mongoosePaginate);
+
+const Reserve = mongoose.model("Reserve", reserveSchema);
+
+module.exports = reserve;
