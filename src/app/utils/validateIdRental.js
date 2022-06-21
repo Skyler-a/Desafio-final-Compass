472de174058699d57094f66524ee8afb780132c0
Skyler-a <skyarthur352@gmail.com>
2022-06-21 17:14:54 -0300

    Refactor: refactored validate rental

--- src/app/utils/validateIdRental.js
+++ src/app/utils/validateIdRental.js
@@ -1,29 +1,29 @@
 const rentalRepository = require('../repository/rentalRepository');
 const notFound = require('../utils/notFound');
 class validationRental {
-    async validateRentalIdPost(idRentalBody, id) {
+    async validateRentalIdPost(idRentalBody, idRentalParam) {
         const rental = await rentalRepository.findRentalById(idRentalBody)
-        const rentalParam = await rentalRepository.findRentalById(id)
+        const rentalParam = await rentalRepository.findRentalById(idRentalParam)
         if (!rental || !rentalParam) {
             throw new notFound("id_rental");
         }
     }
-    async validateRentalId(id) {
-        const rental = await rentalRepository.findRentalById(id)
+    async validateRentalId(idRentalParam) {
+        const rental = await rentalRepository.findRentalById(idRentalParam)
         if (!rental) {
             throw new notFound("id_rental");
         }
     }
-    async validateRentalIdUpdate(idRentalBody, id) {
+    async validateRentalIdUpdate(idRentalBody, idRentalParam) {
         if (idRentalBody) {
             const rental = await rentalRepository.findRentalById(idRentalBody)
-            const rentalParam = await rentalRepository.findRentalById(id)
+            const rentalParam = await rentalRepository.findRentalById(idRentalParam)
             if (!rental || !rentalParam) {
                 throw new notFound("id_rental");
             }
         }
-        if (id) {
-            const rental = await rentalRepository.findRentalById(id)
+        if (idRentalParam) {
+            const rental = await rentalRepository.findRentalById(idRentalParam)
             if (!rental) {
                 throw new notFound("id_rental");
             }
