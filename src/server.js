6b7128d9c515e2d5c7e2bdf5bc3e50a08f3f5622
Skyler-a <skyarthur352@gmail.com>
2022-06-21 19:15:43 -0300

    Fix: heroku port

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const app = require('./app')
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Servidor rodando na porta 3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
 })
