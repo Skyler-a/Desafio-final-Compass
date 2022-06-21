5da46b7e2f74f15967a4383d365f629ab10fc47e
Skyler-a <skyarthur352@gmail.com>
2022-06-21 19:59:43 -0300

    Feat: created reserve update joi validation

---
+++ src/app/middlewares/validReserveUpdate.js
@@ -0,0 +1,25 @@
+const joi = require('joi');
+
+const reservePost = joi.object({
+    id_user: joi.string(),
+    data_start: joi.string(),
+    data_end: joi.string(),
+    id_car: joi.string(),
+    id_rental: joi.string(),
+    final_value: joi.number()
+})
+
+module.exports = async (req, res, next) => {
+    const reqBody = req.body;
+    try {
+
+        if (req.method == "PUT") {
+            await reservePost.validateAsync({ ...reqBody });
+            next();
+        }
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message
+        })
+    }
+}
