6ebefde162e928283747eff0c97b74e554d32d13
Skyler-a <skyarthur352@gmail.com>
2022-06-22 03:56:08 -0300

    Refactor: refactored rental validations

---
+++ src/app/middlewares/validRentalUpdate.js
@@ -0,0 +1,33 @@
+const joi = require('joi');
+const validateCNPJ = require('../utils/validateCnpj');
+
+const rentalUpdate = joi.object({
+    name: joi.string(),
+    cnpj: joi.string(),
+    activities: joi.string(),
+    address: joi.object({
+        cep: joi.string(),
+        number: joi.string(),
+        isFilial: joi.string().valid('true', 'false')
+    }).min(1)
+})
+
+module.exports = async (req, res, next) => {
+    try {
+        const reqBody = req.body;
+        const cnpj = validateCNPJ(reqBody.cnpj)
+        if (!cnpj) {
+            return res.status(400).json({
+                message: "Your cnpj is invalid"
+            })
+        }
+
+        if (req.method == "PUT") {
+            await rentalUpdate.validateAsync({ ...reqBody });
+            next();
+        }
+
+    } catch (error) {
+        return res.status(400).json(error.message)
+    }
+}
