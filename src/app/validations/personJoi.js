3b258357654cbf0509460365b308a914d3e0c3c0
Skyler-a <skyarthur352@gmail.com>
2022-06-26 17:32:52 -0300

    Refactor: regex validation added

--- src/app/validations/personJoi.js
+++ src/app/validations/personJoi.js
@@ -1,9 +1,10 @@
 const joi = require('joi').extend(require('@joi/date'));
 const enums = require('../utils/enums');
+const { cpf } = require('../utils/regex');
 
 const personPost = joi.object({
   name: joi.string().min(4).required(),
-  cpf: joi.string().min(11).max(14).required(),
+  cpf: joi.string().min(11).max(14).regex(cpf).message('Cpf format invalid. Try: 000.000.000-00').required(),
   birthDay: joi.date().format('DD/MM/YYYY').required(),
   email: joi.string().email().required(),
   password: joi.string().min(6).required(),
@@ -15,7 +16,7 @@ const personPost = joi.object({
 
 const personPut = joi.object({
   name: joi.string().min(4),
-  cpf: joi.string().min(11).max(14),
+  cpf: joi.string().min(11).max(14).regex(cpf).message('Cpf format invalid. Try: 000.000.000-00'),
   birthDay: joi.date().format('DD/MM/YYYY'),
   email: joi.string().email(),
   password: joi.string().min(6),
