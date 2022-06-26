cdd5bcfcd7c291f319a811b88ea72339f8392841
Skyler-a <skyarthur352@gmail.com>
2022-06-26 18:42:37 -0300

    Feat: added validations to routes reserve

--- src/routes/reserveRoutes.js
+++ src/routes/reserveRoutes.js
@@ -1,11 +1,13 @@
 const ReserveController = require('../app/controllers/reserveController');
+const ReservePostMiddleware = require('../app/middlewares/validReservePost');
+const ReserveUpdateMiddleware = require('../app/middlewares/validReserveUpdate');
 const idMiddleware = require('../app/middlewares/validIdReserve');
 
 module.exports = (server, routes, prefix = '/api/v1/rental') => {
-  routes.post('/:id/reserve', idMiddleware, ReserveController.createReserve);
+  routes.post('/:id/reserve', idMiddleware, ReservePostMiddleware, ReserveController.createReserve);
   routes.get('/:id/reserve', idMiddleware, ReserveController.getReserve);
   routes.get('/:id/reserve/:id_reserve', idMiddleware, ReserveController.getReserveId);
-  routes.put('/:id/reserve/:id_reserve', idMiddleware, ReserveController.updateReserve);
+  routes.put('/:id/reserve/:id_reserve', idMiddleware, ReserveUpdateMiddleware, ReserveController.updateReserve);
   routes.delete('/:id/reserve/:id_reserve', idMiddleware, ReserveController.deleteReserve);
   server.use(prefix, routes);
 };
